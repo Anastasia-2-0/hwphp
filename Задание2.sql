18eb265847c145afe290bc0ad87b784b5cb0a488
Anastasia-2-0 <nosova0702@mail.ru>
2023-01-12 13:11:13 +0300

    решение

---
+++ Задание2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE studens (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER,
+  adress TEXT NOT NULL
+);
+INSERT INTO studens VALUES (1, 'Clark', 25,'Москва' ),
+                           (2, 'Dave', 37, 'Казахстан'),
+                            (3, 'Ava', 48, 'Нижний Новгород'); 
